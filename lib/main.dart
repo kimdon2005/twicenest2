c8c86a1ad77b90c58ab29f524d039506e6476bd8
kimdon2005 <kimdon2005@naver.com>
2021-12-08 22:35:42 +0900

    still error in building ios and aos

--- lib/main.dart
+++ lib/main.dart
@@ -9,23 +9,18 @@ import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 import 'package:flutter_downloader/flutter_downloader.dart';
 import 'download.dart';
+
 Future<void> main() async {
-    WidgetsFlutterBinding.ensureInitialized();
+  WidgetsFlutterBinding.ensureInitialized();
   await FlutterDownloader.initialize();
-  
-  runApp(MyApp());}
 
+  runApp(MyApp());
+}
 
-
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-     debugShowCheckedModeBanner: false,  
-      home: Example_app()
-
-      
-    );
+    return MaterialApp(debugShowCheckedModeBanner: false, home: Example_app());
   }
 }
 
@@ -34,20 +29,17 @@ class Example_app extends StatefulWidget {
   Example_app({Key? key}) : super(key: key);
 
   @override
-  _Example_appState createState() 
-  => _Example_appState();
+  _Example_appState createState() => _Example_appState();
 }
 
 // ignore: camel_case_types
 class _Example_appState extends State<Example_app> {
   List<String?> list = [];
 
- 
-
   Completer<WebViewController> _controller = Completer<WebViewController>();
- 
-   int progress = 0;
- 
+
+  int progress = 0;
+
   ReceivePort _receivePort = ReceivePort();
 
   static downloadingCallback(id, status, progress) {
@@ -58,19 +50,17 @@ class _Example_appState extends State<Example_app> {
     sendPort!.send([id, status, progress]);
   }
 
-
   @override
   void initState() {
     // ignore: todo
     // TODO: implement initState
     super.initState();
 
-
     if (Platform.isAndroid) WebView.platform = SurfaceAndroidWebView();
 
     ///register a send port for the other isolates
-    IsolateNameServer.registerPortWithName(_receivePort.sendPort, "downloading");
-
+    IsolateNameServer.registerPortWithName(
+        _receivePort.sendPort, "downloading");
 
     ///Listening for the data is comming other isolataes
     _receivePort.listen((message) {
@@ -81,117 +71,88 @@ class _Example_appState extends State<Example_app> {
       print(progress);
     });
 
-
-
     FlutterDownloader.registerCallback(downloadingCallback);
   }
 
-
   @override
   Widget build(BuildContext context) {
-   
-    return  WillPopScope (
-      child : Scaffold(
-      resizeToAvoidBottomInset: false,
-      body:  WebView(
-          initialUrl: 'https://www.twicenest.com/board',
-          javascriptMode: JavascriptMode.unrestricted,
-          onWebViewCreated: (WebViewController webViewController) {
-          _controller.complete(webViewController);
-          _controller.future.then((value) => _controller = value as Completer<WebViewController>);
-          }
-        ),  
-
-       bottomNavigationBar: BottomAppBar(
-        shape: CircularNotchedRectangle(),
-        notchMargin: 4.0,
-        child: Row(
-          mainAxisSize: MainAxisSize.max,
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: <Widget>[
-            backbutton(),
-            _buildShowUrlBtn()
-          ],
+    return WillPopScope(
+        child: Scaffold(
+          resizeToAvoidBottomInset: false,
+          body: WebView(
+              initialUrl: 'https://www.twicenest.com/board',
+              javascriptMode: JavascriptMode.unrestricted,
+              onWebViewCreated: (WebViewController webViewController) {
+                _controller.complete(webViewController);
+                _controller.future.then((value) =>
+                    _controller = value as Completer<WebViewController>);
+              }),
+          bottomNavigationBar: BottomAppBar(
+            shape: CircularNotchedRectangle(),
+            notchMargin: 4.0,
+            child: Row(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[backbutton(), _buildShowUrlBtn()],
+            ),
+          ),
         ),
-      ),  
-       
- 
-      
-      
+        onWillPop: () => onWillPop());
+  }
 
-            ), onWillPop: () 
-            => onWillPop()
-          
-          );
-        }
-      
+  late DateTime backbuttonpressedTime;
 
-   
-   
-    late DateTime backbuttonpressedTime;  
-
-    Future<bool> onWillPop() async {
+  Future<bool> onWillPop() async {
     DateTime currentTime = DateTime.now();
 
     //Statement 1 Or statement2
-   bool backButton = currentTime.difference(backbuttonpressedTime) > Duration(seconds: 3);
+    bool backButton =
+        currentTime.difference(backbuttonpressedTime) > Duration(seconds: 3);
 
-   if (backButton) {
-     backbuttonpressedTime = currentTime;
-     Fluttertoast.showToast(
-        msg: "앱 종료를 원하면 더블 클릭을 하세요!!",
-        backgroundColor: Colors.black,
-        textColor: Colors.white);
-    return false;
+    if (backButton) {
+      backbuttonpressedTime = currentTime;
+      Fluttertoast.showToast(
+          msg: "앱 종료를 원하면 더블 클릭을 하세요!!",
+          backgroundColor: Colors.black,
+          textColor: Colors.white);
+      return false;
     }
-   return true;
+    return true;
   }
-      
-       
-      
-         Widget _buildShowUrlBtn() {
-          return FutureBuilder<WebViewController>(
-            future: _controller.future,
-            builder:
-                (BuildContext context, AsyncSnapshot<WebViewController> controller) {
-              if (controller.hasData) {
-                return IconButton(
-                  onPressed: () async {
-                    var url = await controller.data!.currentUrl();
-                      makeRequest(url);
-                      
-                  },
-                  icon: Icon(Icons.download),
-                );
-              }
-              return Container();
+
+  Widget _buildShowUrlBtn() {
+    return FutureBuilder<WebViewController>(
+      future: _controller.future,
+      builder:
+          (BuildContext context, AsyncSnapshot<WebViewController> controller) {
+        if (controller.hasData) {
+          return IconButton(
+            onPressed: () async {
+              var url = await controller.data!.currentUrl();
+              makeRequest(url);
             },
+            icon: Icon(Icons.download),
           );
         }
+        return Container();
+      },
+    );
+  }
 
-        Widget backbutton() {
-          return FutureBuilder<WebViewController>(
-          future: _controller.future,
-          builder: (BuildContext context,
-              AsyncSnapshot<WebViewController> controller) {
-            if (controller.hasData) {
-              return IconButton(
-                  icon: Icon(Icons.arrow_back),
-                  onPressed: () {
-                    controller.data!.goBack();
-                  });
-            }
-
-            return Container();
+  Widget backbutton() {
+    return FutureBuilder<WebViewController>(
+        future: _controller.future,
+        builder: (BuildContext context,
+            AsyncSnapshot<WebViewController> controller) {
+          if (controller.hasData) {
+            return IconButton(
+                icon: Icon(Icons.arrow_back),
+                onPressed: () {
+                  controller.data!.goBack();
+                });
           }
-      );
-        }
-      
-      
-      
-      }
 
-
-
-
-
+          return Container();
+        });
+  }
+}
